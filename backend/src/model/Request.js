d087720680063a2a4f720fa7ccfb23ad89052a4f
Matheus Reis <matheus201922@hotmail.com>
2026-03-11 22:01:38 -0300

    testing request functions

--- backend/src/model/Request.js
+++ backend/src/model/Request.js
@@ -15,7 +15,6 @@ const Request = sequelize.define("Request", {
   user_id: {
     type: DataTypes.INTEGER,
     allowNull: false,
-    defaultValue: "1",
     references: {
       model: "Users",
       key: "id",
