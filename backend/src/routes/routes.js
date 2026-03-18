361ec827d27184e5dc09e4f9bdc2ba71eb0c8773
mhre1s <matheus201922@hotmail.com>
2026-03-18 17:31:02 -0300

    adding category routes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 const UserController = require("../controllers/UserController");
 const ItemController = require("../controllers/ItemController");
 const RequestController = require("../controllers/RequestController");
+const CategoryController = require("../controllers/CategoryController")
 
 const routes = new Router();
 
@@ -17,5 +18,8 @@ routes.post("/requests", RequestController.store);
 routes.get("/requests", RequestController.index);
 routes.patch("/requests/:id/approve", RequestController.approve);
 routes.patch("/requests/:id/reject", RequestController.reject);
+
+routes.get("/categories", CategoryController.getCategories);
+routes.post("/categories", CategoryController.createCategories)
 
 module.exports = routes;
