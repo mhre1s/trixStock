b2bbbe4e4f4904d69596e669cc33e750214274ac
Matheus Reis <matheus201922@hotmail.com>
2026-03-23 01:20:00 -0300

    test

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const User = require("../models/User");
+const User = require("../model/User");
 
 const register = async (req, res) => {
   try {
