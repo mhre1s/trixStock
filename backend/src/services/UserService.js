f06d20ffcdb30b7f45b0e60c9c6e1cfef6172f21
Matheus Reis <matheus201922@hotmail.com>
2026-03-23 02:23:01 -0300

    fixing auth bugs

--- backend/src/services/UserService.js
+++ backend/src/services/UserService.js
@@ -1,12 +1,21 @@
 const User = require('../model/User')
+const bcrypt = require("bcrypt");
 
-class UserService{
-    async getUsers(){
-        return await User.findAll({order: [["name", "ASC"]]})
-    }
-    async createUser(data){
-        return await User.create(data)
-    }
+class UserService {
+  async getUsers() {
+    return await User.findAll({ order: [["name", "ASC"]] });
+  }
+  async createUser(userData) {
+    // ESSA LINHA É O QUE FALTA:
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+
+    const user = await User.create({
+      ...userData,
+      password: hashedPassword, // Salva a senha protegida
+    });
+    return user
+  }
+  
 }
 
 module.exports = new UserService()
--- frontend/trix-stock/src/apis/registerApi.js
+++ frontend/trix-stock/src/apis/registerApi.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const API_URL = "https://trixstock.onrender.com";
 
 export const registerUser = async (userData) => {
-  const response = await axios.post(`${API_URL}/auth/register`, userData);
+  const response = await axios.post(`${API_URL}/register`, userData);
   return response.data;
 };
